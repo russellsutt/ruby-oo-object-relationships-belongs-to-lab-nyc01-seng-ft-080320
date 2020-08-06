7d21807c5b0833c82d63aa42d652ba7333c40c6a
Russell Sutter <russell.sutter@gmail.com>
2020-08-06 00:01:28 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,11 @@
+require_relative "..lib/song.rb"
+
+class Artist
+  
+  attr_accessor :name, :genre
+  
+  def initialize(name, genre)
+    @name = name
+    @genre = genre
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,12 @@
+require_relative "..lib/artist.rb"
+
+
+class Song
+  
+  attr_accessor :title, :artist
+  
+  def initialize(title, artist)
+    @title = title
+    @artist = artist
+  end
+end
